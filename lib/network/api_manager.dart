ea701b49bdcca2819fdc3174cfdd02a75e85ae3e
Shubham Rao <64189046+SecurFlyInfinity@users.noreply.github.com>
2023-09-18 11:31:35 +0530

    Update api_manager.dart
    updated

--- lib/network/api_manager.dart
+++ lib/network/api_manager.dart
@@ -3,8 +3,11 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 const String baseURL = "https://api.github.com/repos/";
 class ApiManager{
+  
   static Future<http.Response> getCommits() async {
     final response = await http.get( Uri.parse(baseURL + "flutter/flutter/commits"),);
     return response;
   }
+
+  
 }
